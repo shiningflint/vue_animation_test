8db12572e8b9847b21a2959086091a159e1bf246
Adam Christopher <adam.keristoper@gmail.com>
2018-09-03 18:14:04 +0900

    Removing default component, change to mine

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-animation/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyAnimation from './components/MyAnimation.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyAnimation
   }
 }
 </script>
---
+++ src/components/MyAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>
+      <span>bananas</span>
+      <button type="button" @click="deleteItem">delete</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  methods: {
+    deleteItem () {
+      console.log('deleting the item')
+    }
+  }
+}
+</script>
