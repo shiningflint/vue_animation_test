74d291dc6407c553a239029d1f39fa0c7471fe71
Adam Christopher <adam.keristoper@gmail.com>
2018-09-03 18:20:04 +0900

    Adding show potato feature

--- src/components/MyAnimation.vue
+++ src/components/MyAnimation.vue
@@ -1,17 +1,33 @@
 <template>
-  <ul>
-    <li>
-      <span>bananas</span>
-      <button type="button" @click="deleteItem">delete</button>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <span>bananas</span>
+        <button type="button" @click="deleteItem">delete</button>
+      </li>
+    </ul>
+    <button type="button" @click="showPotato(true)">show potatoes</button>
+    <p v-show="isPotatoShown">
+      Here are them potatoes!
+      <button type="button" @click="showPotato(false)">close</button>
+    </p>
+  </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
+  data () {
+    return {
+      isPotatoShown: false
+    }
+  },
   methods: {
     deleteItem () {
       console.log('deleting the item')
+    },
+    showPotato (value) {
+      this.isPotatoShown = value
     }
   }
 }
